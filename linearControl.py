6f4ee580592b57ed7408177b92f8aa6eee8f4e6c
halden718 <attiaa13@gmail.com>
2016-04-04 10:43:40 +0300

    Create linearControl.py

---
+++ linearControl.py
@@ -0,0 +1,29 @@
+import sys
+import time
+
+if __name__ == "__main__":
+  EPSILON = 10**0
+  SLEEP = 0.5
+  MOVEMENT = sys.argv[1]
+  disVec = getDis(MOVEMENT)
+  while(sum(x**2 for x in disVec)) < EPSILON):
+    lr_move(disVec[0], MOVEMENT)
+    fb_move(disVec[1], MOVEMENT)
+    time.sleep(SLEEP)
+    disVec = getDis(MOVEMENT)
+  print('Got to location')
+
+def disVec(movement):
+  """Returns a tuple (x-lr,y-fb) distances."""
+  if movement is 'gps':
+    print('Empty')
+    return (0, 0)
+  if movement is 'img'
+    print('Empty')
+    return (0, 0)
+  if movement is 'test'
+    print('Insert distance')
+    lr = input('lr value: ')
+    fb = input('fb value: ')
+    return (lr, fb)
+    
