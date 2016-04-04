ba3bb83ddf00e8fe4287a1fa426a5a85cf1f22cf
attiaa13@gmail.com <attiaa13@gmail.com>
2016-04-04 21:55:49 +0300

    Update

--- linearControl.py
+++ linearControl.py
@@ -1,14 +1,15 @@
 import sys
 import time
+import picamera
+import imgNav
 
-def getDis(movement):
+def getDis(movement, cam=None):
     """Returns a tuple (x-lr,y-fb) distances."""
     if movement == 'gps':
         print('Empty')
         return (0, 0)
     if movement == 'img':
-        print('Empty')
-        return (0, 0)
+        return imgNav.imgVec(cam)
     if movement == 'test':
         print('Insert distance')
         lr = float(input('lr value: '))
@@ -33,7 +34,10 @@ if __name__ == "__main__":
     EPSILON = 10**0
     SLEEP = 0.5
     MOVEMENT = sys.argv[1]
-    disVec = getDis(MOVEMENT)
+    camera = None
+    if MOVEMENT == 'img':
+        camera = picamera.PiCamera()
+    disVec = getDis(MOVEMENT, camera)
     while(sum(x**2 for x in disVec) > EPSILON):
         lr_move(disVec[0], MOVEMENT)
         fb_move(disVec[1], MOVEMENT)
