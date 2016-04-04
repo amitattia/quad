1fd71d2859de30fafa322cf56d72427ace5d946f
Kelsier <attiaa13@gmail.com>
2016-04-04 11:29:51 +0300

    Update spaces

--- linearControl.py
+++ linearControl.py
@@ -2,34 +2,42 @@ import sys
 import time
 
 def getDis(movement):
-  """Returns a tuple (x-lr,y-fb) distances."""
-  if movement == 'gps':
-    print('Empty')
-    return (0, 0)
-  if movement == 'img':
-    print('Empty')
-    return (0, 0)
-  if movement == 'test':
-    print('Insert distance')
-    lr = float(input('lr value: '))
-    fb = float(input('fb value: '))
-    return (lr, fb)
+    """Returns a tuple (x-lr,y-fb) distances."""
+    if movement == 'gps':
+        print('Empty')
+        return (0, 0)
+    if movement == 'img':
+        print('Empty')
+        return (0, 0)
+    if movement == 'test':
+        print('Insert distance')
+        lr = float(input('lr value: '))
+        fb = float(input('fb value: '))
+        return (lr, fb)
 
 def lr_move(offset, movement):
-  return
+    if(offset > 0):
+        print('l')
+    else:
+        print('r')
+    return
 
 def fb_move(offset, movement):
-  return
+    if(offset > 0):
+        print('f')
+    else:
+        print('b')
+    return
 
 if __name__ == "__main__":
-  EPSILON = 10**0
-  SLEEP = 0.5
-  MOVEMENT = sys.argv[1]
-  disVec = getDis(MOVEMENT)
-  while(sum(x**2 for x in disVec) > EPSILON):
-    lr_move(disVec[0], MOVEMENT)
-    fb_move(disVec[1], MOVEMENT)
-    time.sleep(SLEEP)
+    EPSILON = 10**0
+    SLEEP = 0.5
+    MOVEMENT = sys.argv[1]
     disVec = getDis(MOVEMENT)
-  print('Got to location')
+    while(sum(x**2 for x in disVec) > EPSILON):
+        lr_move(disVec[0], MOVEMENT)
+        fb_move(disVec[1], MOVEMENT)
+        time.sleep(SLEEP)
+        disVec = getDis(MOVEMENT)
+    print('Got to location')
 
