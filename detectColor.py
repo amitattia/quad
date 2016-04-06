bd97d9c2bf3fe4c40b8a2766e0005ff935214d74
halden718 <attiaa13@gmail.com>
2016-04-06 14:20:46 +0300

    Update detectColor.py

--- detectColor.py
+++ detectColor.py
@@ -10,5 +10,11 @@ with picamera.PiCamera() as camera:
     with picamera.array.PiRGBArray(camera) as stream:
         camera.capture(stream, format='bgr')
         # At this point the image is available as stream.array
-        image = stream.array
-        
+        frame = stream.array
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_blue = np.array([140,110,90])
+        upper_blue = np.array([185,210,160])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        cv2.imshow('mask',mask)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
