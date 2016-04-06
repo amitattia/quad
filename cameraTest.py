8bc7023b50820962be1e0eef29dd7993e0023d85
halden718 <attiaa13@gmail.com>
2016-04-06 13:47:52 +0300

    Update cameraTest.py

--- cameraTest.py
+++ cameraTest.py
@@ -1,2 +1,11 @@
 import time
 import picamera
+
+camera = picamera.PiCamera()
+camera.resolution = (640, 480)
+try:
+    camera.start_preview()
+    time.sleep(5)
+    camera.stop_preview()
+finally:
+    camera.close()
