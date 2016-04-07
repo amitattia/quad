29662296d2ee727eeb379edda7ba337907b006f8
halden718 <attiaa13@gmail.com>
2016-04-07 10:53:25 +0300

    Update simpleTest.py

--- simpleTest.py
+++ simpleTest.py
@@ -35,8 +35,8 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
 	key = cv2.waitKey(1) & 0xFF
  
 	# clear the stream in preparation for the next frame
-	rawCapture.truncate(0)
+    rawCapture.truncate(0)
  
 	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+    if key == ord("q"):
+        break
