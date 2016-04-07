4b91bd894404a774002a86b275c6101dbfc44439
halden718 <attiaa13@gmail.com>
2016-04-07 11:10:25 +0300

    Create navigatin.py

---
+++ navigatin.py
@@ -0,0 +1,37 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+import numpy as np
+from scipy import ndimage
+def getCenter(image):
+    return ndimage.measurements.center_of_mass(image)
+ 
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+ 
+# allow the camera to warmup
+time.sleep(0.1)
+ 
+# capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the raw NumPy array representing the image, then initialize the timestamp
+    # and occupied/unoccupied text
+    image = frame.array
+	
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    lower_blue = np.array([100,50,50])
+    upper_blue = np.array([120,255,255])
+    mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    loc = getCenter(mask)
+    print(loc)
+ 
+    cv2.imshow("Frame", mask)
+    key = cv2.waitKey(1) & 0xFF
+    rawCapture.truncate(0)
+    if key == ord("q"):
+        break
