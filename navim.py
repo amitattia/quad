5151cb68aa93c5327af650040b01db0d154fd21a
halden718 <attiaa13@gmail.com>
2016-04-10 14:35:34 +0300

    Update navim.py

--- navim.py
+++ navim.py
@@ -10,12 +10,14 @@ def getCenter(image):
     return ndimage.measurements.center_of_mass(image)
 
 def navigateImg():
+    moveTime = 0.5
+    sleepTime = 2
     camera = PiCamera()
     camera.resolution = (640, 480)
     camera.framerate = 32
     rawCapture = PiRGBArray(camera, size=(640, 480))
-    e1 = 30.00000
-    e2 = 30.00000
+    e1 = 20.00000
+    e2 = 20.00000
     time.sleep(0.1)
     dst = (320, 240)
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
@@ -23,7 +25,7 @@ def navigateImg():
             # grab the raw NumPy array representing the image, then initialize the timestamp
             # and occupied/unoccupied text
             image = frame.array
-        	
+        	w = 0
             hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
             lower_blue = np.array([100,50,50])
             upper_blue = np.array([120,255,255])
@@ -34,24 +36,28 @@ def navigateImg():
             if(abs(dst[1]-loc[1]) > e1):
                 if(dst[1] > loc[1]):
                     print('b')
-                    backwards()
+                    backwards(20+int((dst[1]-loc[1])/3))
                 else:
                     print('f')
-                    forwards()
+                    forwards(20-int((dst[1]-loc[1])/3))
             else:
                 print('fb good')
+                w = w + 1
             if(abs(dst[0]-loc[0]) > e2):
                 if(dst[0] > loc[0]):
                     print('l')
-                    left()
+                    left(20+int((dst[0]-loc[0])/3))
                 else:
                     print('r')
-                    right()
+                    right(20-int((dst[0]-loc[0])/3))
             else:
                 print('lf good')
-            time.sleep(1)
+                w = w + 1
+            if w == 2:
+                print('drop')
+            time.sleep(moveTime)
             zero()
-            time.sleep(2)
+            time.sleep(sleepTime)
             rawCapture.truncate(0)
         except OSError:
             print('io')
