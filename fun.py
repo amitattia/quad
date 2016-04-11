9304cbd9125d7ecf7041c7f1391461883bae29d5
halden718 <attiaa13@gmail.com>
2016-04-11 12:12:40 +0300

    Update fun.py

--- fun.py
+++ fun.py
@@ -7,39 +7,9 @@ def all():
     bus.write_block_data(address, 2, [2, 1, 107])
     bus.write_block_data(address, 2, [3, 6, 148])
 
-def zero():
-    tmp = 0.2
-    bus.write_block_data(address, 2, [1, 4, 0])
-    time.sleep(tmp)
-    bus.write_block_data(address, 2, [0, 4, 0])
-    time.sleep(tmp)
-    bus.write_block_data(address, 2, [2, 4, 0])
-    time.sleep(tmp)
-    bus.write_block_data(address, 2, [3, 4, 0])
-    time.sleep(tmp)
-
-def up():
-    bus.write_block_data(address, 2, [2, 4, 100])
-    time.sleep(0.2)
-
-def down():
-    bus.write_block_data(address, 2, [2, 3, 100])
-    time.sleep(0.2)
-
-def forwards():
-    bus.write_block_data(address, 2, [1, 4, 250])
-    time.sleep(0.2)
-
-def backwards():
-    bus.write_block_data(address, 2, [1, 3, 100])
-    time.sleep(0.2)
-
-def right():
-    bus.write_block_data(address, 2, [0, 3, 100])
-    time.sleep(0.2)
 
 def lift():
-    bus.write_block_data(address, 2, [2, 8, 255])
+    up(250)
     time.sleep(5)
 
 def cool():
@@ -48,27 +18,32 @@ def cool():
     time.sleep(3)
     zero()
     lift()
-    forwards()
+    forwards(150)
     time.sleep(2)
-    backwards()
+    backwards(175)
     time.sleep(1.5)
     for i in range (1,3):
-      right()
+      right(250-50*i)
       time.sleep(0.5*i)
-      backwards()
+      backwards(250-50*i)
       time.sleep(0.5*i)
       if i==2:
-        down()
+        down(100)
       else:
-        up()
+        up(100)
       sleep(0.5)
-      left()
+      left(250-50*i)
       time.sleep(0.5*i)
-      forwards()
+      forwards(250-50*i)
       time.sleep(0.5*i)
       
       
-      
+    for i in range (1,5):
+        forward(200)
+        time.sleep(0.3)
+        backward(200)
+        time.sleep(0.3)
+    
       
 def enter():
     zero()
